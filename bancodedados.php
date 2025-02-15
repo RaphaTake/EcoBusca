c76d304c25638b8326c91ab0581b0f023063d3e3
RaphaTake <rapha.taketa@gmail.com>
2025-02-14 21:09:45 -0300

    first commit

---
+++ bancodedados.php
@@ -0,0 +1,9 @@
+<?php
+
+$servidor = "localhost";
+$usuario = "u883676767_root";
+$senha = "Ecobusca@1234";
+$banco = "u883676767_ecobusca";
+$conexao = mysqli_connect($servidor, $usuario, $senha, $banco);
+
+?>
